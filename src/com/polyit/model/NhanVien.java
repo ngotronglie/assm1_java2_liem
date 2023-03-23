89f4c5548bdcfe08aa91d33891204b9b424b7d36
Ngo Trong Liem <ngotrongliem2004@gmail.com>
2023-03-23 16:58:59 +0700

    java2_Gd1

---
+++ src/com/polyit/model/NhanVien.java
@@ -0,0 +1,61 @@
+
+package com.polyit.model;
+
+public class NhanVien {
+    private String ma, hoTen, email;
+    private double luong;
+    private int tuoi;
+
+    public NhanVien() {
+    }
+
+    public NhanVien(String ma, String hoTen,int tuoi, String email, double luong ) {
+        this.ma = ma;
+        this.hoTen = hoTen;
+        this.email = email;
+        this.luong = luong;
+        this.tuoi = tuoi;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getLuong() {
+        return luong;
+    }
+
+    public void setLuong(double luong) {
+        this.luong = luong;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+    
+    
+}
